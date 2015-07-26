57d173185d4e3c1baf0c7883b5a811ec1e6ae7d4
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-26 13:24:03 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -8,9 +8,11 @@ rm mywebsql-3.6.zip
 
  #apache2 conf
  a2enmod rewrite
- chown -R www-data:www-data /var/www/mywebsql
+ chown -R www-data:www-data /var/www/mywebsql /var/log/apache2
  sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/mywebsql/' /etc/apache2/sites-enabled/000*.conf
- 
+ sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/apache2/php.ini
+ sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php5/apache2/php.ini
+ sed -i -e "s@\$ALLOW_CUSTOM_SERVERS.*@\$ALLOW_CUSTOM_SERVERS = TRUE;@g" /var/www/mywebsql/config/servers.php
  rm -R /var/www/html/
  
  #to fix error relate to ip address of container apache2
